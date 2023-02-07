163c999b21251606a78844e66a958fb59eee6aac
VitaliiMnykh <124521696+VitaliiMnykh@users.noreply.github.com>
2023-02-07 18:50:00 +0100

    Add files via upload

---
+++ src/components/Product.tsx
@@ -0,0 +1,11 @@
+import React from "react"
+import { products } from "../data/products"
+
+    export function Product() {
+        return (
+            <div
+            className="border py-2 px-4 rounded flex flex-col items-center mb-2"
+            >
+         Product!</div>
+        )
+}
